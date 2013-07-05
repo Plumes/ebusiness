62b726e892f46946578b7319642bf35a3c4604ca
Plumes <zhaohonghao2010@gmail.com>
2013-07-05 11:16:15 +0800

    Friday

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+//echo $_GET['kind'];
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,6 +9,37 @@
     <link href="/ebusiness/main.css" rel="stylesheet" media="screen">
     <script type="text/javascript" src="http://upcdn.b0.upaiyun.com/libs/jquery/jquery-2.0.0.min.js"></script>
 	<title>Online Store</title>
+	<div class="navbar navbar-static-top">
+  		<div class="navbar-inner">
+	    <a class="brand" id="mybrand" href="#">Online Store</a>
+	    <ul class="nav">
+	      <li class="active mynav"><a href="/ebusiness/">首页</a></li>
+	      <li class="mynav"><a href="?kind=books" >Books</a></li>
+	      <li class="mynav"><a href="?kind=electronics" >Electronics</a></li>
+	    </ul>
+	    </div>
+	</div>
+	<ul id="maindiv">
+		<li id="ltitle">all products</li>
+		<li id="list" class="plist">
+			<div class="tags">
+				<p><span class="label label-warning">price</span></p>
+				<p><span class="label label-warning">price2</span></p>
+			</div>
+
+			<div class="avatar"><img src="http://making-photos.b0.upaiyun.com/users/55b7d849b26568a63915752c11c69563.jpg!small" alt=""></div>
+			<div class="ptitle">出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走出一个Macbook Air 11寸 2010定制款 3800拿走</div>
+			<div class="pinfo">time</div>
+			
+		</li>
+
+		<li id="list" class="plist">
+			<div class="avatar"></div>
+			<div class="ptitle">i'm product</div>
+			<div class="pinfo">time</div>
+		</li>
+
+	</ul>
 
 </head>
 <body>
